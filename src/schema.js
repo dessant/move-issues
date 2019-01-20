d9e1360fae6c1a6f817a5ddf4c1aa8920b04b0a8
dessant <git@rmn.space>
2019-01-20 10:43:29 +0200

    fix: apply stricter config validation

--- src/schema.js
+++ src/schema.js
@@ -28,11 +28,21 @@ const schema = Joi.object().keys({
     .description('Move labels that also exist on the target repository'),
 
   aliases: Joi.object()
-    .pattern(/.+/, Joi.string())
+    .pattern(
+      Joi.string()
+        .trim()
+        .max(100),
+      Joi.string()
+        .trim()
+        .max(140)
+    )
     .default({})
     .description('Set custom aliases for targets'),
 
-  _extends: Joi.string().description('Repository to extend settings from'),
+  _extends: Joi.string()
+    .trim()
+    .max(260)
+    .description('Repository to extend settings from'),
 
   perform: Joi.boolean().default(!process.env.DRY_RUN)
 });
